4e9716723e2da588b54aae5aa09b3137d7453dc0
sreevani8 <sreevanidumpala@gmail.com>
2025-05-10 12:44:57 +0530

    added change in catalog-service file in dev

--- catalog-service/src/main/java/com/mulit/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/mulit/bookstore/catalog/CatalogServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatalogServiceApplication {
-
+// changed in dev
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
