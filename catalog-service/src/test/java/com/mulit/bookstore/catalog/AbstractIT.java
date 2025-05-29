ff0da52c0065d8072d6fb48c050b45ac20861bab
sreevani8 <sreevanidumpala@gmail.com>
2025-05-29 11:36:55 +0530

    Dockerized catalog service commit

--- catalog-service/src/test/java/com/mulit/bookstore/catalog/AbstractIT.java
+++ catalog-service/src/test/java/com/mulit/bookstore/catalog/AbstractIT.java
@@ -1,6 +1,5 @@
 package com.mulit.bookstore.catalog;
 
-import com.mulit.bookstore.catalog.web.controllers.ContainersConfig;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +7,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import(ContainersConfig.class)
+@Import(TestcontainersConfiguration.class)
 public abstract class AbstractIT {
     @LocalServerPort
     int port;
